944800471f8877d30b389ef808a745ccca482ab4
ahmet-dgn <ahmetcandgn@gmail.com>
2024-05-10 16:38:14 +0300

    query function added

--- app/page.js
+++ app/page.js
@@ -1,30 +1,12 @@
-import Link from "next/link";
-
-async function getPosts() {
-  const response = await fetch(`${process.env.WORDPRESS_API_URL}/posts`);
-  const posts = await response.json();
-  return posts;
-}
+import getData from "@/lib/query";
 
 export default async function Home() {
-  const posts = await getPosts();
-
+  const posts = await getData("menu-items");
+  console.log(posts);
   return (
     <div className="blog-page">
       <h2>All Blog Posts</h2>
-      <p>All blog posts are fetched from WordPress via the WP REST API.</p>
-      <div className="posts">
-        {posts.map((post) => {
-          return (
-            <>
-              <h3>{post.title.rendered}</h3>
-              <p
-                dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-              ></p>
-            </>
-          );
-        })}
-      </div>
+      <p>{posts[0].url}</p>
     </div>
   );
 }
